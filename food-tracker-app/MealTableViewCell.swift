ef968a85ad9d027cc436a694641b7f2d46d41c15
Dipansh <deepanshrocks438@gmail.com>
2019-10-20 12:28:39 +0530

    Add MealTable View and Controller

---
+++ food-tracker-app/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  food-tracker-app
+//
+//  Created by Dipansh Khandelwal on 20/10/19.
+//  Copyright © 2019 DipanshKhandelwal. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
